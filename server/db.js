98b97e63e36c17faa355134e9e10fea8bed01ba1
Timothy Loo <TimothyLoo07@gmail.com>
2022-09-20 11:18:04 -1000

    updated db connection to be ec2 instance

--- server/db.js
+++ server/db.js
@@ -5,6 +5,7 @@ const { Client, Pool } = require('pg');
 const client = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
